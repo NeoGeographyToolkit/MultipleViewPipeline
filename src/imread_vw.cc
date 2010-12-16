55822fda07981de7ba363cb35a0d11865acabd9e
Kyle Husmann <kyle.husmann@gmail.com>
2010-12-15 22:37:12 -0800

    finished imread_vw

--- src/imread_vw.cc
+++ src/imread_vw.cc
@@ -1,4 +1,7 @@
 #include <octave/oct.h>
+
+#include <vw/Image.h>
+#include <vw/FileIO.h>
  
 DEFUN_DLD(imread_vw, args, ,
          "Load an image using VW")
@@ -20,6 +23,27 @@ DEFUN_DLD(imread_vw, args, ,
 
    std::string filename = args(0).string_value();
 
+   vw::ImageView<double> vw_img;
+   try {
+    read_image(vw_img, filename);
+   } catch (vw::Exception& e) {
+    error(e.what());
+    return retval;
+   }
 
-   return octave_value(5);
+   Matrix oct_img(vw_img.rows(), vw_img.cols());
+
+   typedef vw::ImageView<double>::pixel_accessor AccT;
+   AccT racc = vw_img.origin();
+
+   for(int row = 0; row < vw_img.rows(); row++) {
+     AccT cacc = racc;
+     for(int col = 0; col < vw_img.cols(); col++) {
+       oct_img(row, col) = *cacc;
+       cacc.next_col();
+     }
+     racc.next_row();
+   }
+
+   return octave_value(oct_img);
 }
