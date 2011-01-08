a42bc16a9580e1f820a9b390b68bbb47a9791039
Kyle Husmann <kyle.husmann@gmail.com>
2011-01-07 21:06:35 -0800

    added plots, minimization

--- octave/domvp.m
+++ octave/domvp.m
@@ -7,9 +7,27 @@
 pt = [20; 20];
 halfKern = 7;
 
-plane = [0.98447 0.16892 1734793.07299805];
+% plane = [0.98447 0.16892 1734793.07299805];
 
 patchLon = ccrop(demLon, pt, halfKern);
 patchLat = ccrop(demLat, pt, halfKern);
 
-mvpobj_gauss(plane, patchLon, patchLat, orbits)
+%initialPlane = 1734800;
+%[x obj info iter nf lambda] = sqp(initialPlane, @(x) mvpobj_gauss([0.98447 0.16892 x], patchLon, patchLat, orbits), [], [], [], [], 10, 1e-20)
+
+%initialPlane = [0.98; 0.17];
+%[x obj info iter nf lambda] = sqp(initialPlane, @(x) mvpobj_gauss([x; 1734793.07299805], patchLon, patchLat, orbits), [], [], [], [], 10, 1e-20)
+
+%initialPlane = [0.98; 0.17; 1734800];
+%[x obj info iter nf lambda] = sqp(initialPlane, @(x) mvpobj_gauss(x, patchLon, patchLat, orbits), [], [], [], [], 10, 1e-20)
+
+%initialPlane = [0.98; 0.17];
+%function o = obj(plane, patchLon, patchLat, orbits)
+%  o = mvpobj_gauss([plane; 1734792.07299805], patchLon, patchLat, orbits);
+%endfunction
+%[x v nev] = minimize("obj", {initialPlane, patchLon, patchLat, orbits}, "maxev", 54, "verbose", "isz", 0.004)
+
+%initialPlane = [0.9; 0.1];
+%opts(1) = 1; % show progress
+%opts(10) = 40; % Max num of f evals
+%fmins(@(x) mvpobj_gauss([x; 1734793.07299805], patchLon, patchLat, orbits), initialPlane, opts)
---
+++ octave/plotradmvpobj.m
@@ -0,0 +1,21 @@
+demPt = [20; 20];
+halfKern = 7;
+
+n = 20;
+planeLonlat = [0.98447 0.16892];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+patchLon = ccrop(demLon, pt, halfKern);
+patchLat = ccrop(demLat, pt, halfKern);
+
+planeRads = linspace(1734790, 1734800, n);
+
+o = zeros([n 1]);
+for k = 1:n
+  obj = mvpobj_gauss([planeLonlat planeRads(k)], patchLon, patchLat, orbits)
+  o(k) = obj;
+  disp(k);
+endfor
+
+plot(planeRads, o);
