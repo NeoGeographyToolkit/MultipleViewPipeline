c83891a83e25180b675c9da0f030d33cd0da8ba0
Kyle Husmann <kyle.husmann@gmail.com>
2011-01-16 17:15:43 -0800

    vectorized lonlatgrid

--- octave/lonlatgrid.m
+++ octave/lonlatgrid.m
@@ -3,17 +3,32 @@ function [lon lat] = lonlatgrid(georef, sz, off = [1 1])
     sz = [sz sz];
   endif
 
-  lon = zeros(sz);
-  lat = zeros(sz);
+  [X, Y] = meshgrid(off(1):sz(2)+off(1)-1,off(2):sz(1)+off(2)-1);
+  D = [X(:), Y(:), ones(sz(1) * sz(2), 1)]';
+  PD = georef * D;
 
-  for r = 1:sz(1)
-    for c = 1:sz(2)
-      % px -> lonlat
-      ll = georef * [c + off(1) - 1;r + off(2) - 1;1];
-
-      % store in result (and normalize homog coords)
-      lon(r, c) = ll(1) / ll(3);
-      lat(r, c) = ll(2) / ll(3);
-    endfor
-  endfor    
+  lon = reshape(PD(1,:) ./ PD(3,:), sz);
+  lat = reshape(PD(2,:) ./ PD(3,:), sz);
 endfunction
+
+%!test
+%! sz = [4, 3];
+%! off = [5, 3];
+%! georef = [0.00002,  0.00000, 0.98145;
+%!           0.00000, -0.00002, 0.16900;
+%!           0.00030,  0.00200, 1.00000];
+%! [lon_imp lat_imp] = lonlatgrid(georef, sz, off);
+%! for r = 1:sz(1)
+%!   for c = 1:sz(2)
+%!     % px -> lonlat
+%!     ll = georef * [c + off(1) - 1;r + off(2) - 1;1];
+%!
+%!     % store in result (and normalize homog coords)
+%!     lon(r, c) = ll(1) / ll(3);
+%!     lat(r, c) = ll(2) / ll(3);
+%!   endfor
+%! endfor
+%! errLon = norm(lon_imp(:) - lon(:));
+%! errLat = norm(lat_imp(:) - lat(:));
+%! assert(errLon, 0, 1e-6);
+%! assert(errLat, 0, 1e-6);
