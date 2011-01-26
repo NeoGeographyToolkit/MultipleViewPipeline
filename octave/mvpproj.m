a3b46f3e0863defc98e11418f552c7fb4d848485
Kyle Husmann <kyle.husmann@gmail.com>
2011-01-25 20:44:14 -0800

    added plane2normd

--- octave/mvpproj.m
+++ octave/mvpproj.m
@@ -1,9 +1,5 @@
 function patches = mvpproj(plane, demPt, georef, orbits, hWin)
-  cntrPtLonLatH = georef * [demPt(1); demPt(2); 1];
-  cntrPtXYZ = plane(3) * lonlat2normal(cntrPtLonLatH(1:2) / cntrPtLonLatH(3));
-  planeNormal = lonlat2normal(plane(1:2));
-  planeD = dot(planeNormal, cntrPtXYZ);
-
+  [planeNormal planeD] = plane2normd(plane, demPt, georef);
 
   patches = cellfun(@(orbit) porthoproj(planeNormal, planeD, demPt, georef, orbit, hWin), 
                     orbits, "UniformOutput", false);
---
+++ octave/plane2normd.m
@@ -0,0 +1,6 @@
+function [planeNormal planeD] = plane2normd(plane, demPt, georef)
+  cntrPtLonLatH = georef * [demPt(1); demPt(2); 1];
+  cntrPtXYZ = plane(3) * lonlat2normal(cntrPtLonLatH(1:2) / cntrPtLonLatH(3));
+  planeNormal = lonlat2normal(plane(1:2));
+  planeD = dot(planeNormal, cntrPtXYZ);
+endfunction
