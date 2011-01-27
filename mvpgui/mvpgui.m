b45e0393c4a96148eee3de3854bedfe9367a298c
Kyle Husmann <kyle.husmann@gmail.com>
2011-01-26 17:49:07 -0800

    more improved error handling'

--- mvpgui/mvpgui.m
+++ mvpgui/mvpgui.m
@@ -11,25 +11,35 @@ catch
   exit(1);
 end_try_catch
 
-% Load data
+% Load data into workspace (ws)
 printf("Loading images... ");
-[dem georef] = imread_vw(args.demFile);
-dem += DATUM_RADIUS;
+try
+  [ws.dem ws.georef] = imread_vw(args.demFile);
+  ws.dem += DATUM_RADIUS;
 
-for k = 1:args.numOrbs
-  orbs{k}.img = imread_vw(args.imgFiles{k});
-  orbs{k}.cam = loadcam_vw(args.camFiles{k});
-endfor
-printf("Done!\n");
+  for k = 1:args.numOrbs
+    ws.orbs{k}.img = imread_vw(args.imgFiles{k});
+    ws.orbs{k}.cam = loadcam_vw(args.camFiles{k});
+  endfor
+  printf("Done!\n");
+catch
+  printf("\nError loading images: %s", lasterror.message);
+  exit(1);
+end_try_catch
 
-% Show command prompt
+% Enter interactive shell
 while 1
   try
-    act = input("mvp> ", "s");
+    cmd = strsplit(tolower(input("mvp> ", "s")), " ", true);
   catch
     % Ctrl-D exits
     exit(0);
   end_try_catch
+
+  if length(cmd) == 0
+    continue;
+  endif
+
 endwhile
 
 % vim:set syntax=octave:
