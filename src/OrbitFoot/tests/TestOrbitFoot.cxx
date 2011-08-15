f204806bff888653b0fac3c408333e9c22174815
Kyle Husmann <kyle.husmann@gmail.com>
2011-08-15 13:24:34 -0700

    added testing capability to build system

---
+++ src/OrbitFoot/tests/TestOrbitFoot.cxx
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+
+TEST(FootprintTest, FourCorners) {
+  EXPECT_EQ(1, 1);
+}
+
+TEST(FootprintTest, FindTiles) {
+  EXPECT_EQ(1, 1);
+}
+
+TEST(OrbitTreeTest, InsertOrbit) {
+  EXPECT_EQ(1, 1);
+}
