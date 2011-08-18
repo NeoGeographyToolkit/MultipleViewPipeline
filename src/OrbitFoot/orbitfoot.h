7a101c48e1e29f61c0ea332f0a60abb876ed1a8b
Kyle Husmann <kyle.husmann@gmail.com>
2011-08-17 18:12:20 -0700

    basic OrbitFootprint working, not wrapping yet

--- src/OrbitFoot/orbitfoot.h
+++ src/OrbitFoot/orbitfoot.h
@@ -24,17 +24,57 @@ vw::Vector2 backproj_px(vw::camera::PinholeModel const& cam, vw::Vector2 const& 
   return vw::math::subvector(llr, 0, 2);
 }
 
+bool isect_poly(std::vector<vw::Vector2> poly1, std::vector<vw::Vector2> poly2)
+{
+  // Algorithm from http://www.gpwiki.org/index.php/Polygon_Collision
+  // Straight up dumb check, no optimizations attempted...
+  VW_ASSERT(poly1.size() >= 3 && poly2.size() >= 3, vw::LogicErr() << "Invalid polygons");
+
+
+  for (int currVert1 = 0; currVert1 < poly1.size(); currVert1++) {
+    int nextVert1 = currVert1 + 1;
+    nextVert1 = nextVert1 < poly1.size() ? nextVert1: 0;
+
+    vw::Vector2 dir = poly1[nextVert1] - poly1[currVert1];
+    vw::Vector2 perp_dir(-dir[1], dir[0]);
+
+    double poly1_min = std::numeric_limits<double>::max();
+    double poly1_max = std::numeric_limits<double>::min();
+    BOOST_FOREACH(vw::Vector2 v, poly1) {
+      double res = dot_prod(perp_dir, v);
+      poly1_min = std::min(res, poly1_min);
+      poly1_max = std::max(res, poly1_max);
+    }
+
+    double poly2_min = std::numeric_limits<double>::max();
+    double poly2_max = std::numeric_limits<double>::min();
+    BOOST_FOREACH(vw::Vector2 v, poly2) {
+      double res = dot_prod(perp_dir, v);
+      poly2_min = std::min(res, poly2_min);
+      poly2_max = std::max(res, poly2_max);
+    }
+
+    if (poly1_min > poly2_max || poly1_max < poly2_min) {
+      return false;
+    }
+  }
+
+  return true;  
+}
+
 class OrbitFootprint
 {
+  const static int m_tile_size = 256;
   std::string m_camfile;
   vw::Vector2i m_img_size;
   std::vector<vw::Vector2> m_vertices;
+  vw::BBox2 m_bbox;
 
   public:
     OrbitFootprint(std::string const& camfile, 
                    vw::Vector2i const& img_size, 
                    double sphere_rad) :
-      m_camfile(camfile), m_img_size(img_size), m_vertices(4)
+      m_camfile(camfile), m_img_size(img_size), m_vertices(4), m_bbox()
     {
       vw::camera::PinholeModel cam(camfile);
 
@@ -43,10 +83,56 @@ class OrbitFootprint
       m_vertices[1] = backproj_px(cam, vw::Vector2i(img_size.x(), 0), sphere_rad);
       m_vertices[2] = backproj_px(cam, vw::Vector2i(img_size.x(), img_size.y()), sphere_rad);
       m_vertices[3] = backproj_px(cam, vw::Vector2i(0, img_size.y()), sphere_rad);
+
+      // Create a BBox for the footprint
+      BOOST_FOREACH(vw::Vector2 v, m_vertices) {
+        m_bbox.grow(v);
+      }
+
+      // TODO: Take care of wrapping around the poles!
+      // For now just warn
+      vw::BBox2 bounds(-180,180,360,-360);
+      BOOST_FOREACH(vw::Vector2 v, m_vertices) {
+        if (!bounds.contains(v)) {
+          std::cout << v << " Ooops this vertex will wrap! fixme!" << std::endl;
+        }
+      }
+    }
+
+    vw::BBox2 bounding_box() {
+      return m_bbox;
     }
 
     bool intersects(vw::BBox2 tile) {
-      return true;
+      std::vector<vw::Vector2> tile_poly(4);
+      tile_poly[0] = tile.min();
+      tile_poly[1] = vw::Vector2(tile.min().x(), tile.max().y());
+      tile_poly[2] = tile.max();
+      tile_poly[3] = vw::Vector2(tile.max().x(), tile.min().y());
+
+      return isect_poly(m_vertices, tile_poly);
+    }
+
+    bool intersects(int col, int row, int level) {
+      vw::BBox2 tile_box(col * m_tile_size, row * m_tile_size, m_tile_size, m_tile_size);
+
+      // Resolution at the current level, in degrees per pixel
+      double resolution = m_tile_size << level;
+
+      // Create a transform
+      // TODO: us a prebuilt georef?
+      vw::Matrix3x3 transform;
+      transform(0,0) = 360.0 / resolution;
+      transform(0,2) = -180.0;
+      transform(1,1) = -360.0 / resolution;
+      transform(1,2) = 180.0;
+      transform(2,2) = 1;
+
+      vw::Vector2 newbox_min = dehom(transform * hom(tile_box.min()));
+      vw::Vector2 newbox_max = dehom(transform * hom(tile_box.max()));
+
+      vw::BBox2 newbox(newbox_min, newbox_max);
+      return this->intersects(newbox);
     }
 };
 
--- src/OrbitFoot/tests/TestOrbitFoot.cxx
+++ src/OrbitFoot/tests/TestOrbitFoot.cxx
@@ -101,5 +101,44 @@ TEST_F(OrbitFootprintTest, Intersects) {
   //Vector2(172.639,-26.9718)
   //                              Vector2(178.42,-27.6722)
 
-  EXPECT_TRUE(ofp.intersects(BBox2()));
+  EXPECT_TRUE(ofp.intersects(BBox2(175, -25, 5, 5)));
+  EXPECT_FALSE(ofp.intersects(BBox2(180, -25, 5, 5)));
+
+  EXPECT_TRUE(ofp.intersects(0, 0, 0));
+  EXPECT_FALSE(ofp.intersects(0, 0, 20));
+
+  EXPECT_TRUE(ofp.intersects(63, 36, 6));
+  EXPECT_FALSE(ofp.intersects(63, 37, 6));
+}
+
+TEST(IsectPolyTest, Intersects) {
+  using namespace vw;
+  std::vector<Vector2> poly1, poly2, poly3, poly4;
+
+  poly1.push_back(Vector2(30, 40));
+  poly1.push_back(Vector2(30, 80));
+  poly1.push_back(Vector2(50, 80));
+  poly1.push_back(Vector2(50, 40));
+
+  poly2.push_back(Vector2(40, 30));
+  poly2.push_back(Vector2(40, 70));
+  poly2.push_back(Vector2(60, 70));
+  poly2.push_back(Vector2(60, 30));
+
+  poly3.push_back(Vector2(45, 50));
+  poly3.push_back(Vector2(45, 60));
+  poly3.push_back(Vector2(47, 60));
+  poly3.push_back(Vector2(47, 50));
+
+  poly4.push_back(Vector2(48, 20));
+  poly4.push_back(Vector2(48, 60));
+  poly4.push_back(Vector2(70, 60));
+  poly4.push_back(Vector2(70, 20));
+
+  EXPECT_TRUE(isect_poly(poly1, poly2));
+  EXPECT_TRUE(isect_poly(poly1, poly3));
+  EXPECT_TRUE(isect_poly(poly2, poly3));
+  EXPECT_TRUE(isect_poly(poly1, poly4));
+  EXPECT_TRUE(isect_poly(poly2, poly4));
+  EXPECT_FALSE(isect_poly(poly3, poly4));
 }
