d44d76ac9dbf5713af1951b00d0f05d1a0f8dd58
Kyle Husmann <kyle.husmann@gmail.com>
2011-09-21 12:52:30 -0700

    Added testing, added new TileDescriptor to CMakelists

---
+++ src/mvp/tests/TestMVPWorkspace.cxx
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <test/Helpers.h>
+
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+
+//#include "MVPWorkspace.h"
+
+namespace fs = boost::filesystem;
+
+TEST(IsectPolyTest, Intersects) {
+  EXPECT_TRUE(5==5);
+}
