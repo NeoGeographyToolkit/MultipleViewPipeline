94a157e608660d056b92fb6e09fbf1901436d908
Kyle Husmann <kyle.husmann@gmail.com>
2011-09-21 18:55:13 -0700

    filled in construct_footprint and added helper functions / tests

--- src/mvp/OrbitalImage.h
+++ src/mvp/OrbitalImage.h
@@ -12,12 +12,69 @@
 #include "OrbitalImageDesc.pb.h"
 
 #include <vw/Cartography/Datum.h>
+#include <vw/Cartography/SimplePointImageManipulation.h>
 #include <vw/Camera/PinholeModel.h>
 #include <vw/FileIO/DiskImageResource.h>
 
 #include <boost/foreach.hpp>
 
 namespace mvp {
+
+vw::Vector2 backproj_px(vw::camera::PinholeModel const& cam, vw::Vector2 const& px, double sphere_rad)
+{
+  vw::Vector3 dir = cam.pixel_to_vector(px);
+  vw::Vector3 cntr = cam.camera_center();
+
+  double a = dot_prod(dir, dir);
+  double b = 2 * dot_prod(cntr, dir);
+  double c = dot_prod(cntr, cntr) - sphere_rad * sphere_rad;
+
+  double t = (-b - sqrt(b*b-4*a*c))/2/a;
+
+  vw::Vector3 isect = cntr + t * dir;
+
+  vw::Vector3 llr = vw::cartography::xyz_to_lon_lat_radius(isect);
+
+  return vw::math::subvector(llr, 0, 2);
+}
+
+bool isect_poly(std::vector<vw::Vector2> poly1, std::vector<vw::Vector2> poly2)
+{
+  // Algorithm from http://www.gpwiki.org/index.php/Polygon_Collision
+  // Straight up dumb check, no optimizations attempted...
+  VW_ASSERT(poly1.size() >= 3 && poly2.size() >= 3, vw::LogicErr() << "Invalid polygons");
+
+
+  for (int currVert1 = 0; currVert1 < poly1.size(); currVert1++) {
+    int nextVert1 = currVert1 + 1;
+    nextVert1 = nextVert1 < poly1.size() ? nextVert1: 0;
+
+    vw::Vector2 dir = poly1[nextVert1] - poly1[currVert1];
+    vw::Vector2 perp_dir(-dir[1], dir[0]);
+
+    double poly1_min = std::numeric_limits<double>::max();
+    double poly1_max = std::numeric_limits<double>::min();
+    BOOST_FOREACH(vw::Vector2 v, poly1) {
+      double res = dot_prod(perp_dir, v);
+      poly1_min = std::min(res, poly1_min);
+      poly1_max = std::max(res, poly1_max);
+    }
+
+    double poly2_min = std::numeric_limits<double>::max();
+    double poly2_max = std::numeric_limits<double>::min();
+    BOOST_FOREACH(vw::Vector2 v, poly2) {
+      double res = dot_prod(perp_dir, v);
+      poly2_min = std::min(res, poly2_min);
+      poly2_max = std::max(res, poly2_max);
+    }
+
+    if (poly1_min > poly2_max || poly1_max < poly2_min) {
+      return false;
+    }
+  }
+
+  return true;  
+}
 
 class OrbitalImage
 {
@@ -81,8 +138,34 @@ class OrbitalImage
     static std::vector<vw::Vector2> construct_footprint(vw::camera::PinholeModel const& camera, vw::Vector2i const& image_size,
                                                         vw::Vector2 const& radius_range)
     {
-      // TODO
-      return std::vector<vw::Vector2>();
+      std::vector<vw::Vector2> fp;
+
+      // TODO: Take into account the radius range!
+      fp.push_back(backproj_px(camera, vw::Vector2i(0, 0), radius_range[0]));
+      fp.push_back(backproj_px(camera, vw::Vector2i(image_size.x(), 0), radius_range[0]));
+      fp.push_back(backproj_px(camera, vw::Vector2i(image_size.x(), image_size.y()), radius_range[0]));
+      fp.push_back(backproj_px(camera, vw::Vector2i(0, image_size.y()), radius_range[0]));
+
+      // TODO: Need to take care of wrapping around poles!
+      vw::BBox2 maxbounds(-180,-180,360,360);
+      BOOST_FOREACH(vw::Vector2 v, fp) {
+        if (!maxbounds.contains(v)) {
+          VW_ASSERT(maxbounds.contains(v), vw::NoImplErr() << "Lonlat wrapping in footprints needs to be implemented");
+        }
+      }
+
+      return fp;
+    }
+
+    /// Return a lonlat bounding box for the footprint of the orbital image
+    vw::BBox2 footprint_bbox() const {
+      vw::BBox2 bbox;
+
+      BOOST_FOREACH(vw::Vector2 v, m_footprint) {
+        bbox.grow(v);
+      }
+
+      return bbox;
     }
 
     /// Reset the radius range for the footprint
--- src/mvp/tests/TestOrbitalImage.cxx
+++ src/mvp/tests/TestOrbitalImage.cxx
@@ -14,6 +14,60 @@ using namespace vw::test;
 using namespace vw::camera;
 using namespace vw::cartography;
 
+TEST(HelperFunction, backproj_px) {
+  double moon_rad = 1737400;
+
+  PinholeModel cam(vw::Vector3(-1664843.82,116630.703,-771606.935),
+    vw::Quat(0.6196358355,-0.3398896742,0.413973702,-0.5737178878).rotation_matrix(),
+    3802.7,3802.7,2862.875,2862.875,
+    vw::Vector3(1,0,0),
+    vw::Vector3(0,1,0),
+    vw::Vector3(0,0,1),
+    vw::camera::NullLensDistortion());
+
+  Vector2 px_pick(100.0, 200.0);
+
+  Vector2 lonlat = backproj_px(cam, px_pick, moon_rad);
+
+  Vector3 llr(lonlat[0], lonlat[1], moon_rad);
+  Vector3 xyz = vw::cartography::lon_lat_radius_to_xyz(llr);
+
+  Vector2 px_pick_again = cam.point_to_pixel(xyz);
+
+  EXPECT_VECTOR_NEAR(px_pick, px_pick_again, 1e-6);
+}
+
+TEST(HelperFunction, isec_poly) {
+  vector<Vector2> poly1, poly2, poly3, poly4;
+
+  poly1.push_back(Vector2(30, 40));
+  poly1.push_back(Vector2(30, 80));
+  poly1.push_back(Vector2(50, 80));
+  poly1.push_back(Vector2(50, 40));
+
+  poly2.push_back(Vector2(40, 30));
+  poly2.push_back(Vector2(40, 70));
+  poly2.push_back(Vector2(60, 70));
+  poly2.push_back(Vector2(60, 30));
+
+  poly3.push_back(Vector2(45, 50));
+  poly3.push_back(Vector2(45, 60));
+  poly3.push_back(Vector2(47, 60));
+  poly3.push_back(Vector2(47, 50));
+
+  poly4.push_back(Vector2(48, 20));
+  poly4.push_back(Vector2(48, 60));
+  poly4.push_back(Vector2(70, 60));
+  poly4.push_back(Vector2(70, 20));
+
+  EXPECT_TRUE(isect_poly(poly1, poly2));
+  EXPECT_TRUE(isect_poly(poly1, poly3));
+  EXPECT_TRUE(isect_poly(poly2, poly3));
+  EXPECT_TRUE(isect_poly(poly1, poly4));
+  EXPECT_TRUE(isect_poly(poly2, poly4));
+  EXPECT_FALSE(isect_poly(poly3, poly4));
+}
+
 class OrbitalImageTest : public ::testing::Test {
   protected:
     virtual void SetUp() {
@@ -51,19 +105,39 @@ TEST_F(OrbitalImageTest, build_desc) {
 }
 
 TEST_F(OrbitalImageTest, construct_footprint) {
+  vector<Vector2> fp(OrbitalImage::construct_footprint(PinholeModel(camfile), imgsize, radius_range));
+
+  EXPECT_EQ(fp.size(), 4);
+  EXPECT_VECTOR_NEAR(fp[0], Vector2(173.541,-21.7811), 1e-3);
+  EXPECT_VECTOR_NEAR(fp[1], Vector2(179.133,-22.5502), 1e-3);
+  EXPECT_VECTOR_NEAR(fp[2], Vector2(178.42,-27.6722), 1e-3);
+  EXPECT_VECTOR_NEAR(fp[3], Vector2(172.639,-26.9718), 1e-3);
+}
+
+TEST_F(OrbitalImageTest, footprint_bbox) {
   // TODO
 }
 
 TEST_F(OrbitalImageTest, set_radius_range) {
-  // TODO 
+  Vector2 newrad(1736400, 1738400);
+  OrbitalImage orbimg(camfile, imgsize, radius_range);
+  OrbitalImage orbimg2(camfile, imgsize, newrad);
+
+  orbimg.set_radius_range(newrad);
+ 
+  EXPECT_EQ(orbimg.build_desc().DebugString(), orbimg2.build_desc().DebugString()); 
 }
 
 TEST_F(OrbitalImageTest, equal_resolution_level) {
-  // TODO 
+  OrbitalImage orbimg(camfile, imgsize, radius_range);
+
+  EXPECT_EQ(orbimg.equal_resolution_level(), 6);
 }
 
 TEST_F(OrbitalImageTest, equal_density_level) {
-  // TODO 
+  OrbitalImage orbimg(camfile, imgsize, radius_range);
+
+  EXPECT_EQ(orbimg.equal_density_level(), 20);
 }
 
 TEST_F(OrbitalImageTest, intersects) {
