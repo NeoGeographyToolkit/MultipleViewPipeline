f027e4018f424ceb6a3baafbcd458903d8bfb009
Kyle Husmann <kyle.husmann@gmail.com>
2011-10-08 22:37:09 -0700

    moved add_octfile into FindOctave

--- cmake/modules/FindOctave.cmake
+++ cmake/modules/FindOctave.cmake
@@ -3,6 +3,18 @@
 # Note that ALL_CXXFLAGS contains INCFLAGS, CXXFLAGS and XTRA_CXXFLAGS
 #           ALL_LDFLAGS contains LD_STATIC_FLAG, CPICFLAG, and LDFLAGS
 
+macro(ADD_OCTFILE _octfile _sources)
+  add_library(${_octfile} MODULE ${_sources})
+  target_link_libraries(${_octfile} ${OCTAVE_LIBRARIES})
+
+  set_target_properties(${_octfile} PROPERTIES
+                        LINK_FLAGS "${OCTAVE_LINK_FLAGS}"
+                        COMPILE_FLAGS "${OCTAVE_COMPILE_FLAGS}"
+                        PREFIX ""
+                        SUFFIX ".oct"
+                       )
+endmacro()
+
 include(FindPackageHandleStandardArgs)
 include(FindPackageMessage)
 
