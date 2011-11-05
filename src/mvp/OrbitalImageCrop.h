7d87a7ed9571a98f60d66d8fcf4999e0b5e715f2
Kyle Husmann <kyle.husmann@gmail.com>
2011-11-05 01:36:19 -0700

    OrbitalImageCrop now cropping working, and a test for it

--- src/mvp/OrbitalImageCrop.h
+++ src/mvp/OrbitalImageCrop.h
@@ -36,13 +36,74 @@ vw::camera::PinholeModel offset_pinhole(vw::camera::PinholeModel const& cam, vw:
 class OrbitalImageCrop {
   vw::ImageView<vw::PixelMask<vw::PixelGray<vw::float32> > > m_image;
   vw::camera::PinholeModel m_camera;
+  bool m_isempty;
 
   public:
-    template <class ViewT>
-    OrbitalImageCrop(vw::ImageViewBase<ViewT> const& image, vw::camera::PinholeModel camera) : m_image(image.impl()), m_camera(camera) {}
+
+    static OrbitalImageCrop construct_from_descriptor(OrbitalImageFileDescriptor const& image_file,
+                                                      vw::BBox2 const& lonlat_bbox,
+                                                      vw::cartography::Datum const& datum,
+                                                      vw::Vector2 const& post_height_limits) {
+
+      boost::scoped_ptr<vw::DiskImageResource> rsrc(vw::DiskImageResource::open(image_file.image_path()));
+      vw::BBox2i imagebox(0, 0, rsrc->cols(), rsrc->rows());
+
+      vw::Vector2 radius_range(post_height_limits + vw::Vector2(datum.semi_major_axis(), datum.semi_major_axis()));
+
+      vw::camera::PinholeModel camera(image_file.camera_path());
+
+      std::vector<vw::Vector3> llr_bound_pts(8);
+      llr_bound_pts[0] = vw::Vector3(lonlat_bbox.min()[0], lonlat_bbox.min()[1], radius_range[0]);
+      llr_bound_pts[1] = vw::Vector3(lonlat_bbox.min()[0], lonlat_bbox.max()[1], radius_range[0]);
+      llr_bound_pts[2] = vw::Vector3(lonlat_bbox.max()[0], lonlat_bbox.max()[1], radius_range[0]);
+      llr_bound_pts[3] = vw::Vector3(lonlat_bbox.max()[0], lonlat_bbox.min()[1], radius_range[0]);
+      llr_bound_pts[4] = vw::Vector3(lonlat_bbox.min()[0], lonlat_bbox.min()[1], radius_range[1]);
+      llr_bound_pts[5] = vw::Vector3(lonlat_bbox.min()[0], lonlat_bbox.max()[1], radius_range[1]);
+      llr_bound_pts[6] = vw::Vector3(lonlat_bbox.max()[0], lonlat_bbox.max()[1], radius_range[1]);
+      llr_bound_pts[7] = vw::Vector3(lonlat_bbox.max()[0], lonlat_bbox.min()[1], radius_range[1]);
+
+      vw::BBox2 cropbox;
+      BOOST_FOREACH(vw::Vector3 const& llr, llr_bound_pts) {
+        vw::Vector3 xyz(vw::cartography::lon_lat_radius_to_xyz(llr));
+        cropbox.grow(camera.point_to_pixel(xyz));
+      }
+
+      cropbox.crop(imagebox);
+
+      // Return empty if smaller than a pixel
+      if (cropbox.width() < 1 || cropbox.height() < 1) {
+        return OrbitalImageCrop();
+      }
+
+      cropbox = vw::grow_bbox_to_int(cropbox);
+
+      vw::ImageView<vw::PixelMask<vw::PixelGray<vw::float32> > > image;
+
+      // TODO: Need a better way of determining alpha in VW
+      switch(rsrc->format().pixel_format) {
+        case vw::VW_PIXEL_GRAYA:
+        case vw::VW_PIXEL_SCALAR_MASKED:
+        case vw::VW_PIXEL_GRAY_MASKED:
+          image = vw::crop(vw::DiskImageView<vw::PixelMask<vw::PixelGray<vw::float32> > >(image_file.image_path()), cropbox);
+          break;
+        default:
+          image = vw::crop(create_mask(vw::DiskImageView<vw::PixelGray<vw::float32> >(image_file.image_path()), 
+                                       std::numeric_limits<vw::float32>::quiet_NaN()), cropbox);
+      }
+
+      return OrbitalImageCrop(image, offset_pinhole(camera, cropbox.min()));
+    }
+
 
     vw::ImageView<vw::PixelMask<vw::PixelGray<vw::float32> > > image() const {return m_image;}
     vw::camera::PinholeModel camera() const {return m_camera;}
+    bool isempty() const {return m_isempty;}
+
+  protected:
+    // Make sure the user doesn't construct one
+    OrbitalImageCrop() : m_image(), m_camera(), m_isempty(true) {}
+    template <class ViewT>
+    OrbitalImageCrop(vw::ImageViewBase<ViewT> const& image, vw::camera::PinholeModel camera) : m_image(image.impl()), m_camera(camera), m_isempty(false) {}
 };
 
 class OrbitalImageCropCollection : public std::vector<OrbitalImageCrop> {
@@ -59,46 +120,9 @@ class OrbitalImageCropCollection : public std::vector<OrbitalImageCrop> {
     }
 
     void add_image(OrbitalImageFileDescriptor const& image_file) {
-      boost::scoped_ptr<vw::DiskImageResource> rsrc(vw::DiskImageResource::open(image_file.image_path()));
-      vw::BBox2i imagebox(0, 0, rsrc->cols(), rsrc->rows());
-
-      vw::Vector2 radius_range(m_post_height_limits + vw::Vector2(m_datum.semi_major_axis(), m_datum.semi_major_axis()));
-
-      vw::camera::PinholeModel camera(image_file.camera_path());
-
-      std::vector<vw::Vector3> llr_bound_pts(8);
-      llr_bound_pts[0] = vw::Vector3(m_lonlat_bbox.min()[0], m_lonlat_bbox.min()[1], radius_range[0]);
-      llr_bound_pts[1] = vw::Vector3(m_lonlat_bbox.min()[0], m_lonlat_bbox.max()[1], radius_range[0]);
-      llr_bound_pts[2] = vw::Vector3(m_lonlat_bbox.max()[0], m_lonlat_bbox.max()[1], radius_range[0]);
-      llr_bound_pts[3] = vw::Vector3(m_lonlat_bbox.max()[0], m_lonlat_bbox.min()[1], radius_range[0]);
-      llr_bound_pts[4] = vw::Vector3(m_lonlat_bbox.min()[0], m_lonlat_bbox.min()[1], radius_range[1]);
-      llr_bound_pts[5] = vw::Vector3(m_lonlat_bbox.min()[0], m_lonlat_bbox.max()[1], radius_range[1]);
-      llr_bound_pts[6] = vw::Vector3(m_lonlat_bbox.max()[0], m_lonlat_bbox.max()[1], radius_range[1]);
-      llr_bound_pts[7] = vw::Vector3(m_lonlat_bbox.max()[0], m_lonlat_bbox.min()[1], radius_range[1]);
-
-      vw::BBox2 cropbox;
-      BOOST_FOREACH(vw::Vector3 const& llr, llr_bound_pts) {
-        vw::Vector3 xyz(vw::cartography::lon_lat_radius_to_xyz(llr));
-        cropbox.grow(camera.point_to_pixel(xyz));
-      }
-
-      cropbox.crop(imagebox);
-
-      vw::ImageView<vw::PixelMask<vw::PixelGray<vw::float32> > > image;
-
-      if (cropbox.width() > 1e-6 && cropbox.height() > 1e-6) {
-        // TODO: Need a better way of determining alpha in VW
-        switch(rsrc->format().pixel_format) {
-          case vw::VW_PIXEL_GRAYA:
-          case vw::VW_PIXEL_SCALAR_MASKED:
-          case vw::VW_PIXEL_GRAY_MASKED:
-            image = vw::crop(vw::DiskImageView<vw::PixelMask<vw::PixelGray<vw::float32> > >(image_file.image_path()), vw::grow_bbox_to_int(cropbox));
-            break;
-          default:
-            image = vw::crop(create_mask(vw::DiskImageView<vw::PixelGray<vw::float32> >(image_file.image_path()), std::numeric_limits<vw::float32>::quiet_NaN()), 
-                             vw::grow_bbox_to_int(cropbox));
-        }
-        push_back(OrbitalImageCrop(image, offset_pinhole(camera, cropbox.min())));
+      OrbitalImageCrop image(OrbitalImageCrop::construct_from_descriptor(image_file, m_lonlat_bbox, m_datum, m_post_height_limits));
+      if (!image.isempty()) {
+        push_back(image);
       }
     }
     
--- src/mvp/tests/TestOrbitalImageCrop.cxx
+++ src/mvp/tests/TestOrbitalImageCrop.cxx
@@ -18,16 +18,75 @@ TEST(HelperFunction, offset_pinhole) {
   EXPECT_VECTOR_NEAR(cam.pixel_to_vector(Vector2(20, 30)), cam_off.pixel_to_vector(Vector2(10, 10)), 1e-6);
 }
 
-TEST(OrbtialImageCrop, crop) {
-  // TODO: Actually test the cropping ability
-  /*
+TEST(OrbtialImageCropCollection, crop) {
+/* Footprint for synth.0.pinhole with alt +-10000
+
+    Vector2(56.1474,9.90343)
+                                           Vector2(56.6319,9.69012)
+                                                                  
+                                           
+                                           
+                                                                      Vector2(56.8948,9.53431)
+
+Vector2(55.9392,9.4308)
+
+
+
+
+
+                                 Vector2(56.4232,9.21995)
+                                                                  Vector2(56.7208,9.14307)
+*/
   OrbitalImageFileDescriptor image_file;
 
-  image_file.set_camera_path(SrcName("synth.0.pinhole"));
   image_file.set_image_path(SrcName("synth.0.tif"));
+  image_file.set_camera_path(SrcName("synth.0.pinhole"));
 
-  OrbitalImageCrop orbitalimagecrop(OrbitalImageCrop::construct_from_descriptor(image_file, BBox2(), Datum("D_MOON"), Vector2()));
+  Datum datum("D_MOON");
+  Vector2 alt_range(-10000, 10000);
+  
+  VW_ASSERT(datum.semi_major_axis() == datum.semi_minor_axis(), LogicErr() << "Spheroid datums not supported");
+  Vector2 radius_range(alt_range + Vector2(datum.semi_major_axis(), datum.semi_major_axis()));
 
-  EXPECT_EQ(orbitalimagecrop.image().cols(), 460);
-  EXPECT_EQ(orbitalimagecrop.image().rows(), 460); */
+  ImageView<PixelMask<PixelGray<float32> > > uncrop_image(create_mask(DiskImageView<PixelGray<float32> >(image_file.image_path()), 
+                                                                      numeric_limits<float32>::quiet_NaN()));
+
+  PinholeModel uncrop_camera(image_file.camera_path());
+
+  vector<BBox2> boxes(4);
+
+  boxes[0] = BBox2(55.9392, 9.21995, 0.1, 0.1); // outside footprint
+  boxes[1] = BBox2(55.9392, 9.21995, 0.1, 0.3); // overlap footprint
+  boxes[2] = BBox2(56.4232, 9.4308, 0.1, 0.1); // inside footprint
+  boxes[3] = BBox2(55, 9, 1, 1); // entire footprint
+
+  const int num_pts = 20;
+  BOOST_FOREACH(BBox2 const& b, boxes) {
+    OrbitalImageCrop image_crop(OrbitalImageCrop::construct_from_descriptor(image_file, b, datum, alt_range));
+
+    for (int i = 0; i < num_pts; i++) {
+      for (int j = 0; j < num_pts; j++) {
+        for (int k = 0; k < num_pts; k++) {
+          double lon = b.width() * i / num_pts + b.min().x();
+          double lat = b.height() * j / num_pts + b.min().y();
+          double rad = (radius_range[1] - radius_range[0]) * k / num_pts + radius_range[0];
+          Vector3 llr(lon, lat, rad);
+          Vector3 xyz(lon_lat_radius_to_xyz(llr));
+
+          Vector2 crop_px(image_crop.camera().point_to_pixel(xyz));
+          Vector2 uncrop_px(uncrop_camera.point_to_pixel(xyz));
+
+          // If the uncropped image contains the pixel, then the crop MUST have it, and be the same value
+          // If the uncropped image doesn't contain the pixel, the crop can have it or not.
+          if (bounding_box(uncrop_image).contains(uncrop_px)) {
+            bool crop_contains = bounding_box(image_crop.image()).contains(crop_px);
+            EXPECT_TRUE(crop_contains);
+            if (crop_contains) {
+              EXPECT_TYPE_EQ(image_crop.image()(crop_px.x(), crop_px.y()), uncrop_image(uncrop_px.x(), uncrop_px.y()));
+            }
+          }
+        }
+      }
+    }
+  }
 }
