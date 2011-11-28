152961dbbfb5d695ff0b8ae4dccf613e46ae9155
Kyle Husmann <kyle.husmann@gmail.com>
2011-11-28 12:33:49 -0800

    minor cosmetic code change

--- src/mvp/octave/lonlatalt2xyz.m
+++ src/mvp/octave/lonlatalt2xyz.m
@@ -1,7 +1,8 @@
 function xyz = lonlatalt2xyz(datum, lonlat, alt)
-  if (datum.semi_major_axis == datum.semi_minor_axis)
-    xyz = (datum.semi_major_axis + alt) * lonlat2normal(lonlat);
-  else
+  if (datum.semi_major_axis != datum.semi_minor_axis)
     error("Spheroid datums not supported");
   endif
+
+  xyz = (datum.semi_major_axis + alt) * lonlat2normal(lonlat);
+
 endfunction
