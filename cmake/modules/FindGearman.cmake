6329fc791c05605b8f1c2619c8cb8992563a7db8
Kyle Husmann <kyle.husmann@gmail.com>
2012-02-27 14:18:33 -0800

    added a GEARMAN_ROOT search option to FindGearman

--- cmake/modules/FindGearman.cmake
+++ cmake/modules/FindGearman.cmake
@@ -1,6 +1,12 @@
-find_path(GEARMAN_INCLUDE_DIR libgearman/gearman.h)
+if (NOT "$ENV{GEAR<AN_ROOT}" STREQUAL "")
+  set(GEARMAN_ROOT $ENV{GEARMAN_ROOT})
+endif()
 
-find_library(GEARMAN_LIBRARY NAMES gearman)
+# TODO: if ${GEARMAN_ROOT} is empty, the hint is /include rather than nothing
+# it would be better to do _search_paths like FindBoost does.
+find_path(GEARMAN_INCLUDE_DIR libgearman/gearman.h HINTS ${GEARMAN_ROOT}/include)
+
+find_library(GEARMAN_LIBRARY NAMES gearman HINTS ${GEARMAN_ROOT}/lib)
 
 set(GEARMAN_LIBRARIES ${GEARMAN_LIBRARY})
 set(GEARMAN_INCLUDE_DIRS ${GEARMAN_INCLUDE_DIR})
