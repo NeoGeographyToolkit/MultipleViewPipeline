2fda7c651375ff2586bbe954f8011f151f74e6c4
Kyle Husmann <kyle.husmann@gmail.com>
2012-03-02 10:54:43 -0800

    set default TolX to 1e-2

--- src/mvp/octave/mvpoptimset.m
+++ src/mvp/octave/mvpoptimset.m
@@ -3,7 +3,7 @@ function options = mvpoptimset(alt_range)
   options.fix_orientation = true;
   options.fix_windows = true;
   options.max_iterations = 80;
-  options.alt_tolerance = 0.5;
+  options.alt_tolerance = 1e-2;
   options.fast_reflectance = true;
 endfunction
 
