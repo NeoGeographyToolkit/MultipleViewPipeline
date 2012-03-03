171b2c818f284900b2c162fde828ebda4da4b046
Kyle Husmann <kyle.husmann@gmail.com>
2012-03-03 09:39:40 -0800

    added a derivitive gaussian kernel

--- src/mvp/octave/gausskernel.m
+++ src/mvp/octave/gausskernel.m
@@ -1,4 +1,4 @@
-function kern = gausskernel(sigma, sz)
+function [kern dkern] = gausskernel(sigma, sz)
   if (sigma == "kernsize")
     % A gaussian kernel needs 6sigma values
     kern = round(6 * sz);
@@ -15,12 +15,25 @@ function kern = gausskernel(sigma, sz)
     sz = gausskernel("kernsize", sigma);
   endif
 
-  h = sz - 1;
-  x = (0:h) - h / 2;
-  gauss = exp(-(x.^2) / (2*sigma^2));
-  kern = gauss / sum(gauss(:)); 
+  x = (0:sz) - (sz / 2);
+  kern = diff(normcdf(x, 0, sigma));
+  kern /= sum(kern);
+
+  if (nargout > 1)
+    dkern = diff(normpdf(x, 0, sigma));
+    dkern /= sum(kern);
+  endif 
+
 endfunction
 
-%%TODO: test me?
+%!test
+%! sz = 8;
+%! sigma = 3;
+%! h = sz - 1;
+%! x = (0:h) - h / 2;
+%! gauss = exp(-(x.^2) / (2*sigma^2));
+%! kernref = gauss / sum(gauss(:));
+%! kern = gausskernel(sigma, sz);
+%! assert(kernref, kern, 1e-3)
 
 % vim:set syntax=octave:
