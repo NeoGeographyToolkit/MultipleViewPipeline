65e6bfc183b25a367da5dc9a54c50ec89300da51
Kyle Husmann <kyle.husmann@gmail.com>
2012-03-26 11:39:18 -0700

    updated mvpoptimset to reflect new algorithm options

--- src/mvp/octave/mvpoptimset.m
+++ src/mvp/octave/mvpoptimset.m
@@ -3,7 +3,7 @@ function options = mvpoptimset(alt_range)
   options.fix_orientation = true;
   options.fix_windows = true;
   options.max_iterations = 80;
-  options.alt_tolerance = 1e-2;
+  options.gauss_divisor = 6;
   options.fast_reflectance = true;
 endfunction
 
