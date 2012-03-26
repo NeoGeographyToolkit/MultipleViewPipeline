9669226b6fd33f6e4ae3807d8050b7e35ef69b1a
Kyle Husmann <kyle.husmann@gmail.com>
2012-03-25 17:32:06 -0700

    mvp.conf parsing works in mvpdumpjob now

--- src/mvp/mvpdumpjob.cc
+++ src/mvp/mvpdumpjob.cc
@@ -5,12 +5,41 @@
 
 #include <boost/program_options.hpp>
 
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
 #include <boost/tokenizer.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
 // This should probably be put where others can use it
 namespace mvp {
+
+
+std::vector<std::string> paths_from_pattern(boost::filesystem::path pattern) {
+  namespace fs = boost::filesystem;
+
+  fs::path dir(pattern.parent_path());
+  boost::regex filter(pattern.filename().string());
+
+  std::vector<std::string> result_paths;
+
+  fs::directory_iterator end_itr;
+  for(fs::directory_iterator i(dir); i != end_itr; i++) {
+    if (!fs::is_regular_file(i->status())) {
+      continue;
+    }
+
+    boost::smatch what;
+    if (boost::regex_match(i->path().filename().string(), what, filter)) {
+      result_paths.push_back(i->path().string());
+    }  
+  }
+
+  sort(result_paths.begin(), result_paths.end());
+
+  return result_paths;
+}
+
 
 #define PROPERTY_TO_PROTO(ptree, protobuf, type, proto_name, prop_name) \
 {boost::optional<type> val = ptree.get_optional<type>(prop_name); \
@@ -42,6 +71,24 @@ MVPWorkspaceRequest work_request_from_conf(std::string const& filename) {
   PROPERTY_TO_PROTO(pt, *work_request.mutable_user_settings(), int, max_iterations, "algorithm.max-iterations");
 
   // orbital_images
+  {
+    std::vector<std::string> image_paths = paths_from_pattern(pt.get<std::string>("dataset.orbital-image-pattern"));
+    std::vector<std::string> camera_paths = paths_from_pattern(pt.get<std::string>("dataset.camera-pattern"));
+
+    if (image_paths.size() != camera_paths.size()) {
+      vw::vw_throw(vw::InputErr() << "The number of orbital images does not match the number of cameras");
+    }
+
+    std::vector<std::string>::const_iterator image_iter, camera_iter;
+    for (image_iter = image_paths.begin(), camera_iter = camera_paths.begin();
+         image_iter != image_paths.end();
+         image_iter++, camera_iter++) {
+      OrbitalImageFileDescriptor image;
+      image.set_image_path(*image_iter);
+      image.set_camera_path(*camera_iter);
+      *work_request.add_orbital_images() = image;
+    }
+  }
 
   // render_bbox
   boost::optional<std::string> render_bbox_string = pt.get_optional<std::string>("render.render-bbox");
