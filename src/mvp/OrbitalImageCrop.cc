NeoGeographyToolkit/MultipleViewPipeline

commit 5d93010845f956fd71703126ee925bc0b5d02156

Moved cropping bbox calc outside static constructor

src/mvp/OrbitalImageCrop.h
@@ -56,24 +56,13 @@ class OrbitalImageCrop : public vw::ImageView<vw::PixelMask<vw::PixelGray<vw::fl
     }
   }
   public:
-    static OrbitalImageCrop construct_from_paths(std::string const& image_path,
-                                                 std::string const& camera_path) {
-      boost::shared_ptr<vw::DiskImageResource> rsrc(vw::DiskImageResource::open(image_path));
-      return OrbitalImageCrop(rsrc_helper(rsrc), vw::camera::PinholeModel(camera_path));
-    }
-
-    static OrbitalImageCrop construct_from_paths(std::string const& image_path,
-                                                 std::string const& camera_path,
-                                                 vw::BBox2 const& lonlat_bbox,
-                                                 vw::cartography::Datum const& datum,
-                                                 vw::Vector2 const& alt_limits) {
-
-      boost::shared_ptr<vw::DiskImageResource> rsrc(vw::DiskImageResource::open(image_path));
+    static vw::BBox2i find_crop_bbox(vw::camera::PinholeModel const& camera, 
+                                     vw::BBox2 const& lonlat_bbox,
+                                     vw::cartography::Datum const& datum, 
+                                     vw::Vector2 const& alt_limits) {
 
       VW_ASSERT(datum.semi_major_axis() == datum.semi_minor_axis(), vw::LogicErr() << "Spheroid datums not supported");
       vw::Vector2 radius_range(alt_limits + vw::Vector2(datum.semi_major_axis(), datum.semi_major_axis()));
-
-      vw::camera::PinholeModel camera(camera_path);
 
       std::vector<vw::Vector3> llr_bound_pts(8);
       llr_bound_pts[0] = vw::Vector3(lonlat_bbox.min()[0], lonlat_bbox.min()[1], radius_range[0]);
@@ -91,6 +80,25 @@ class OrbitalImageCrop : public vw::ImageView<vw::PixelMask<vw::PixelGray<vw::fl
         cropbox.grow(camera.point_to_pixel(xyz));
       }
 
+      return cropbox;
+    }
+
+    static OrbitalImageCrop construct_from_paths(std::string const& image_path,
+                                                 std::string const& camera_path) {
+      boost::shared_ptr<vw::DiskImageResource> rsrc(vw::DiskImageResource::open(image_path));
+      return OrbitalImageCrop(rsrc_helper(rsrc), vw::camera::PinholeModel(camera_path));
+    }
+
+    static OrbitalImageCrop construct_from_paths(std::string const& image_path,
+                                                 std::string const& camera_path,
+                                                 vw::BBox2 const& lonlat_bbox,
+                                                 vw::cartography::Datum const& datum,
+                                                 vw::Vector2 const& alt_limits) {
+
+      boost::shared_ptr<vw::DiskImageResource> rsrc(vw::DiskImageResource::open(image_path));
+      vw::camera::PinholeModel camera(camera_path);
+
+      vw::BBox2i cropbox(find_crop_bbox(camera, lonlat_bbox, datum, alt_limits));
       cropbox.crop(vw::BBox2i(0, 0, rsrc->cols(), rsrc->rows()));
 
       // Return empty if smaller than a pixel

src/mvp/tests/TestOrbitalImageCrop.cxx
@@ -18,6 +18,10 @@ TEST(HelperFunction, crop_pinhole) {
   EXPECT_VECTOR_NEAR(cam.pixel_to_vector(Vector2(20, 30)), crop_cam.pixel_to_vector(Vector2(10, 10)), 1e-6);
 }
 
+TEST(OrbitalImageCrop, find_crop_bbox) {
+  // TODO: write me
+}
+
 TEST(OrbtialImageCrop, construct_from_paths) {
 /* Footprint for synth.0.pinhole with alt +-10000
 

commit 4ab7653b5454e4be15ee07d43ce6dbd506f708fd

fixed truncating error introduced by 5d93010845

src/mvp/OrbitalImageCrop.cc
@@ -19,10 +19,10 @@ static vw::ImageViewRef<vw::PixelMask<vw::PixelGray<vw::float32> > > rsrc_helper
 
 namespace mvp {
 
-vw::BBox2i OrbitalImageCrop::find_crop_bbox(vw::camera::PinholeModel const& camera, 
-                                            vw::BBox2 const& lonlat_bbox,
-                                            vw::cartography::Datum const& datum, 
-                                            vw::Vector2 const& alt_limits) 
+vw::BBox2 OrbitalImageCrop::find_crop_bbox(vw::camera::PinholeModel const& camera, 
+                                           vw::BBox2 const& lonlat_bbox,
+                                           vw::cartography::Datum const& datum, 
+                                           vw::Vector2 const& alt_limits) 
 {
   VW_ASSERT(datum.semi_major_axis() == datum.semi_minor_axis(), vw::LogicErr() << "Spheroid datums not supported");
   vw::Vector2 radius_range(alt_limits + vw::Vector2(datum.semi_major_axis(), datum.semi_major_axis()));
@@ -62,7 +62,7 @@ OrbitalImageCrop OrbitalImageCrop::construct_from_paths(std::string const& image
   boost::shared_ptr<vw::DiskImageResource> rsrc(vw::DiskImageResource::open(image_path));
   vw::camera::PinholeModel camera(camera_path);
 
-  vw::BBox2i cropbox(find_crop_bbox(camera, lonlat_bbox, datum, alt_limits));
+  vw::BBox2 cropbox(find_crop_bbox(camera, lonlat_bbox, datum, alt_limits));
   cropbox.crop(vw::BBox2i(0, 0, rsrc->cols(), rsrc->rows()));
 
   // Return empty if smaller than a pixel

src/mvp/OrbitalImageCrop.h
@@ -42,10 +42,10 @@ class OrbitalImageCrop : public vw::ImageView<vw::PixelMask<vw::PixelGray<vw::fl
   vw::camera::PinholeModel m_camera;
 
   public:
-    static vw::BBox2i find_crop_bbox(vw::camera::PinholeModel const& camera, 
-                                     vw::BBox2 const& lonlat_bbox,
-                                     vw::cartography::Datum const& datum, 
-                                     vw::Vector2 const& alt_limits);
+    static vw::BBox2 find_crop_bbox(vw::camera::PinholeModel const& camera, 
+                                    vw::BBox2 const& lonlat_bbox,
+                                    vw::cartography::Datum const& datum, 
+                                    vw::Vector2 const& alt_limits);
 
     static OrbitalImageCrop construct_from_paths(std::string const& image_path,
                                                  std::string const& camera_path);