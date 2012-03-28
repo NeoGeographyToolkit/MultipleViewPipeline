7a5ea6557c66f16a41f390951f11ef0d6c454a19
Kyle Husmann <kyle.husmann@gmail.com>
2012-03-28 01:05:11 -0700

    added test for TileSeeder

--- src/mvp/tests/TestMVPTileSeeder.cxx
+++ src/mvp/tests/TestMVPTileSeeder.cxx
@@ -14,7 +14,7 @@ using namespace mvp;
 
 struct MVPAlgoTest : public MVPAlgorithm {
   virtual MVPPixelResult operator()(MVPAlgorithmVar const& seed, vw::cartography::GeoReference const& georef, MVPAlgorithmOptions const& options) const {
-    return MVPPixelResult();
+    return MVPPixelResult(seed);
   }
 };
 
@@ -23,4 +23,15 @@ TEST(MVPTileSeederDumbTest, init) {
   MVPTileSeederDumb seeder(&algorithm, GeoReference(Datum("D_MOON")), 256, MVPUserSettings());
 
   EXPECT_TRUE(seeder.init());
+}
+
+TEST(MVPTileSeederSquareTest, seed) {
+  MVPAlgoTest algorithm;
+  MVPTileSeederSquare seeder(&algorithm, GeoReference(Datum("D_MOON")), 256, MVPUserSettings());
+
+  seeder.update(0, 0, MVPAlgorithmVar(3));
+  seeder.update(0, 1, MVPAlgorithmVar(4));
+  seeder.update(0, 2, MVPAlgorithmVar(5));
+
+  EXPECT_NEAR(seeder.seed(0, 0).alt, 4, 1e-3);
 }
