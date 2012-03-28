bcc45a387cc7afc7d3cbb64fea182683e8852934
Kyle Husmann <kyle.husmann@gmail.com>
2012-03-27 23:25:52 -0700

    fixed inheritance on MVPTileProcessor

--- src/mvp/MVPTileProcessor.h
+++ src/mvp/MVPTileProcessor.h
@@ -16,16 +16,17 @@ namespace mvp {
 
 class MVPTileSeeder;
 
-struct MVPTileProcessor {
-  virtual MVPTileResult operator()(vw::ProgressCallback const& progress = vw::ProgressCallback::dummy_instance()) const = 0;
+class MVPTileProcessor {
+  protected:
+    MVPTileSeeder *m_seeder;
+  public:
+    MVPTileProcessor(MVPTileSeeder *seeder) : m_seeder(seeder) {}
+    virtual MVPTileResult operator()(vw::ProgressCallback const& progress = vw::ProgressCallback::dummy_instance()) const = 0;
 };
 
-class MVPTileProcessorDumb : public MVPTileProcessor {
-  MVPTileSeeder *m_seeder;
-
-  public:
-    MVPTileProcessorDumb(MVPTileSeeder *seeder) : m_seeder(seeder) {}
-    virtual MVPTileResult operator()(vw::ProgressCallback const& progress = vw::ProgressCallback::dummy_instance()) const;  
+struct MVPTileProcessorDumb : public MVPTileProcessor {
+  MVPTileProcessorDumb(MVPTileSeeder *seeder) : MVPTileProcessor(seeder) {}
+  virtual MVPTileResult operator()(vw::ProgressCallback const& progress = vw::ProgressCallback::dummy_instance()) const;  
 };
 
 } // namespace mvp
