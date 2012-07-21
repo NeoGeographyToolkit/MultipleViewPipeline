2715e2ca7b7a24687901b96b834ae9de2a9e1121
Kyle Husmann <kyle.husmann@gmail.com>
2012-07-21 12:18:40 -0700

    started skeleton for SettingsParser

---
+++ src/mvp/Core/SettingsParser.cc
@@ -0,0 +1,7 @@
+#include <mvp/Core/SettingsParser.h>
+
+namespace mvp {
+namespace core {
+
+}} // namespace core,mvp
+
---
+++ src/mvp/Core/SettingsParser.h
@@ -0,0 +1,18 @@
+/// \file SettingsParser.h
+///
+/// SettingsParser class
+///
+/// ...desc...
+///
+///
+
+#ifndef __MVP_CORE_SETTINGSPARSER_H__
+#define __MVP_CORE_SETTINGSPARSER_H__
+
+namespace mvp {
+namespace core {
+
+
+}} // namespace core, mvp
+
+#endif
