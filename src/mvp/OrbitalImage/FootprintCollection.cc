9f7369023085b17ae6668fec22e9bccd9bfae091
Kyle Husmann <kyle.husmann@gmail.com>
2012-08-22 02:47:58 -0700

    Added FootprintCollection class

---
+++ src/mvp/OrbitalImage/FootprintCollection.cc
@@ -0,0 +1,54 @@
+#include <mvp/OrbitalImage/FootprintCollection.h>
+
+#include <boost/foreach.hpp>
+
+namespace mvp {
+namespace orbitalimage {
+
+void FootprintCollection::push_back_pattern(std::string const& image_pattern, std::string const& camera_pattern) {
+  std::cout << "Add image pattern!" << std::endl;
+}
+
+int FootprintCollection::equal_resolution_level() const {
+  int result = std::numeric_limits<int>::max();
+
+  BOOST_FOREACH(Footprint const& fp, *this) {
+    result = std::min(result, fp.equal_resolution_level());
+  }
+
+  return result;
+}
+
+int FootprintCollection::equal_density_level(int tile_size) const {
+  int result = 0;
+
+  BOOST_FOREACH(Footprint const& fp, *this) {
+    result = std::max(result, fp.equal_density_level(tile_size));
+  }
+
+  return result;
+}
+
+vw::BBox2 FootprintCollection::lonlat_bbox() const {
+  vw::BBox2 result;
+
+  BOOST_FOREACH(Footprint const& fp, *this) {
+    result.grow(fp.bounding_box());
+  }
+
+  return result;
+}
+
+std::vector<OrbitalImage> FootprintCollection::images_in_region(vw::BBox2 const& lonlat_bbox) const {
+  std::vector<OrbitalImage> result;
+
+  BOOST_FOREACH(Footprint const& fp, *this) {
+    if (fp.intersects(lonlat_bbox)) {
+      result.push_back(fp.orbital_image());
+    } 
+  }
+
+  return result;
+}
+
+}} // namespace orbitalimage,mvp
---
+++ src/mvp/OrbitalImage/tests/TestFootprintCollection.cxx
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include <test/Helpers.h>
+#include <mvp/OrbitalImage/FootprintCollection.h>
+
+#include <vw/Image.h>
+#include <vw/Camera.h>
+#include <vw/FileIO.h>
+#include <vw/Cartography.h>
+
+using namespace std;
+using namespace mvp::orbitalimage;
+using namespace vw;
+using namespace vw::test;
+using namespace vw::camera;
+using namespace vw::cartography;
+
+TEST(FootprintCollection, push_back) {
+  FootprintCollection footprints(Datum("D_MOON"), Vector2());
+
+  footprints.push_back(SrcName("data/synth.0.tif"), SrcName("data/synth.0.pinhole"));
+
+  EXPECT_EQ(footprints.size(), 1u);
+  EXPECT_EQ(footprints.equal_resolution_level(), 9);
+  EXPECT_EQ(footprints.equal_density_level(256), 11);
+  EXPECT_VECTOR_NEAR(footprints.lonlat_bbox().min(), Vector2(56.1308, 9.18129), 1e-3);
+  EXPECT_VECTOR_NEAR(footprints.lonlat_bbox().max(), Vector2(56.7642, 9.80684), 1e-3);
+
+  footprints.push_back(SrcName("data/synth.1.tif"), SrcName("data/synth.1.pinhole"));
+  footprints.push_back(SrcName("data/synth.2.tif"), SrcName("data/synth.2.pinhole"));
+  footprints.push_back(SrcName("data/synth.3.tif"), SrcName("data/synth.3.pinhole"));
+
+  EXPECT_EQ(footprints.size(), 4u);
+  EXPECT_EQ(footprints.equal_resolution_level(), 9);
+  EXPECT_EQ(footprints.equal_density_level(256), 11);
+  EXPECT_VECTOR_NEAR(footprints.lonlat_bbox().min(), Vector2(56.0486, 9.18129), 1e-3);
+  EXPECT_VECTOR_NEAR(footprints.lonlat_bbox().max(), Vector2(56.7642, 9.84013), 1e-3);
+}
+
+TEST(FootprintCollection, push_back_pattern) {
+  // TODO: Test me!!
+}
+
+TEST(FootprintCollection, images_in_region) {
+  //TODO: Test me!!
+}
