cf77985e477b88b1c22d49d68af53fbe79cbdaf3
Kyle Husmann <kyle.husmann@gmail.com>
2012-09-17 13:15:25 -0700

    Started Conversions.h

---
+++ src/mvp/Octave/Conversions.h
@@ -0,0 +1,72 @@
+/// \file Conversions.h
+///
+/// Conversions class
+///
+
+#ifndef __MVP_OCTAVE_CONVERSIONS_H__
+#define __MVP_OCTAVE_CONVERSIONS_H__
+
+#include <octave/oct.h>
+#include <octave/oct-map.h>
+
+#include <vw/Math/Vector.h>
+#include <vw/Math/Matrix.h>
+
+namespace mvp {
+namespace octave {
+
+VW_DEFINE_EXCEPTION(BadCastErr, vw::Exception);
+
+/// numeric type -> octave
+template <class ToT, class FromT>
+typename boost::enable_if<boost::is_arithmetic<FromT>, ToT>::type  
+octave_cast(FromT const& v) {
+  return ToT(double(v));
+}
+
+/// octave -> numeric type
+template <class ToT>
+typename boost::enable_if<boost::is_arithmetic<ToT>, ToT>::type
+octave_cast(octave_value const& v) {
+  VW_ASSERT(v.is_scalar_type(), BadCastErr() << "Not a scalar type");
+  return ToT(v.double_value());
+}
+
+/// vw::vector -> octave
+template <class ToT, class VectorT>
+ToT octave_cast(vw::math::VectorBase<VectorT> const& v) {
+  VectorT vw_vect(v.impl());
+  ColumnVector oct_vect(vw_vect.size());
+
+  for (unsigned i = 0; i < vw_vect.size(); i++) {
+    oct_vect(i) = vw_vect[i];
+  }
+
+  return oct_vect;
+}
+
+/// octave -> vw::vector
+template <class VectorT>
+typename boost::enable_if<boost::is_base_of<vw::math::VectorBase<VectorT>, VectorT>, VectorT>::type
+octave_cast(octave_value const& v) {
+  VW_ASSERT(v.is_matrix_type(), BadCastErr() << "Not a matrix type");
+
+  VectorT vw_vect;
+  ColumnVector oct_vect = v.column_vector_value();
+
+  if (vw_vect.size() == 0) {
+    vw_vect = VectorT(oct_vect.length());
+  }
+
+  VW_ASSERT(vw_vect.size() == unsigned(oct_vect.length()), BadCastErr() << "Bad vector size");
+
+  for (int i = 0; i < oct_vect.length(); i++) {
+    vw_vect[i] = oct_vect(i);
+  }
+
+  return vw_vect;
+}
+
+}} // namespace octave, mvp
+
+#endif
---
+++ src/mvp/Octave/tests/TestConversions.cxx
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include <test/Helpers.h>
+
+#include <mvp/Octave/Conversions.h>
+
+using namespace mvp::octave;
+using namespace vw::test;
+using namespace std;
+
+TEST(Conversions, scalar_to_octave) {
+  EXPECT_TRUE((octave_cast<octave_value>(5) == octave_value(5)).bool_value());
+}
+
+TEST(Conversions, octave_to_scalar) {
+  EXPECT_EQ(octave_cast<int>(octave_value(5)), 5);
+}
+
+TEST(Conversions, octave_to_scalar_throws) {
+  EXPECT_THROW(octave_cast<int>(octave_map()), BadCastErr);
+}
+
+TEST(Conversions, vector_to_octave) {
+  vw::Vector3 vw_vect(10, 20, 30);
+  ColumnVector oct_vect(octave_cast<ColumnVector>(vw_vect));
+
+  for (unsigned i = 0; i < vw_vect.size(); i++) {
+    EXPECT_EQ(vw_vect[i], oct_vect(i));
+  }
+}
+
+TEST(Conversions, octave_to_vector) {
+  ColumnVector oct_vect(3);
+  oct_vect(0) = 10; oct_vect(1) = 20; oct_vect(2) = 30;
+
+  vw::Vector3 vw_vect(octave_cast<vw::Vector3>(oct_vect));
+
+  for (unsigned i = 0; i < vw_vect.size(); i++) {
+    EXPECT_EQ(vw_vect[i], oct_vect(i));
+  }
+}
+
+TEST(Conversions, octave_to_vector0) {
+  ColumnVector oct_vect(3);
+  oct_vect(0) = 10; oct_vect(1) = 20; oct_vect(2) = 30;
+
+  vw::Vector<double, 0> vw_vect(octave_cast<vw::Vector<double, 0> >(oct_vect));
+
+  for (unsigned i = 0; i < vw_vect.size(); i++) {
+    EXPECT_EQ(vw_vect[i], oct_vect(i));
+  }
+}
+
+TEST(Conversions, octave_to_vector_throws) {
+  EXPECT_THROW(octave_cast<vw::Vector2>(ColumnVector(3)), BadCastErr);
+  EXPECT_THROW(octave_cast<vw::Vector2>(octave_map()), BadCastErr);
+}
