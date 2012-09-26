bc43d4b4651825511f2210da9b7d16e064926cae
Kyle Husmann <kyle.husmann@gmail.com>
2012-09-25 17:10:38 -0700

    Updated fix_pyproto for mac

--- cmake/fix_pyproto.sh
+++ cmake/fix_pyproto.sh
@@ -1,2 +1,13 @@
-#!/bin/sh
-sed -e 's/^\(\s*import\s*"\)\(.*\)\(\/.*\)/\1\L\2\E\3/' $1 > $2
+#!/bin/bash
+
+SED=sed
+
+if [[ "`uname`" == "Darwin" ]]; then
+  SED=gsed
+fi
+
+# Rename modules to lowercase:
+# import "mvp/Frontend/SessionDesc.proto";
+# becomes
+# import "mvp/frontend/SessionDesc.proto";
+$SED -e 's/^\(\s*import\s*"\)\(.*\)\(\/.*\)/\1\L\2\E\3/' $1 > $2
