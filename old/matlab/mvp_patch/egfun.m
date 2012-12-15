cfd7b5731c7f7660cb46f4143c698df281386bdb
Taemin Kim <luminans@gmail.com>
2012-12-15 15:42:18 -0800

    Correct the gradients of egfun.m

--- old/matlab/mvp_patch/egfun.m
+++ old/matlab/mvp_patch/egfun.m
@@ -1,14 +1,14 @@
 function [f,g,d,F,G] = egfun(x)
-% exp(F1+F2+F3)
-% F1 =   x1^2+1
-% F2 = 2*x2^2+2
-% F1 =  -x2^3-1
+% f = exp(f1+f2+f3)
+% f1 =   x1^2+1
+% f2 = 2*x2^2+2
+% f3 =  -x2^3-1
 x = x(:);
 A = [1 0; 0 2; 0 -1];
 b = [1 2 -1]';
 F = A*x.^2 + b;
-G = 2*A*x;
+G = 2*A;
 f = exp(sum(F));
 d = [f f f]';
-g = f*sum(G);
+g = G'*d;
 end
