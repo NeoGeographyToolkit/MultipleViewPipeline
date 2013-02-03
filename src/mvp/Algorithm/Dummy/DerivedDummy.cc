14c9dd41890f2184b591db7caa230bfd804b1bb5
Kyle Husmann <kyle.husmann@gmail.com>
2013-02-03 10:04:12 -0800

    Split deriveddummy into cc/h

---
+++ src/mvp/Algorithm/Dummy/DerivedDummy.cc
@@ -0,0 +1,30 @@
+#include <mvp/Algorithm/Dummy/DerivedDummy.h>
+
+namespace mvp {
+namespace algorithm {
+
+DerivedDummy::DerivedDummy() : m_x(0), m_y(0) {}
+
+DerivedDummy::DerivedDummy(int x) : m_x(x), m_y(0) {}
+
+DerivedDummy::DerivedDummy(int x, int y) : m_x(x), m_y(y) {}
+
+void DerivedDummy::void0() {}
+
+void DerivedDummy::void1(int a) {}
+
+void DerivedDummy::void2(int a, int b) {}
+
+int DerivedDummy::function0() { return 0; }
+
+int DerivedDummy::function1(int a) { return a; }
+
+int DerivedDummy::function2(int a, int b) { return a + b; }
+
+int DerivedDummy::x() { return m_x; }
+
+int DerivedDummy::y() { return m_y; }
+
+vw::Vector2 DerivedDummy::do_vector(vw::Vector3 const& a) { return vw::math::subvector(a, 0, 2); }
+
+}} // namespace algorithm,mvp
--- src/mvp/Algorithm/Dummy/DerivedDummy.h
+++ src/mvp/Algorithm/Dummy/DerivedDummy.h
@@ -10,29 +10,29 @@ struct DerivedDummy : public Dummy {
   int m_x;
   int m_y;
 
-  DerivedDummy() : m_x(0), m_y(0) {}
+  DerivedDummy();
 
-  DerivedDummy(int x) : m_x(x), m_y(0) {}
+  DerivedDummy(int x);
 
-  DerivedDummy(int x, int y) : m_x(x), m_y(y) {}
+  DerivedDummy(int x, int y);
 
-  void void0() {}
+  void void0();
 
-  void void1(int a) {}
+  void void1(int a);
 
-  void void2(int a, int b) {}
+  void void2(int a, int b);
 
-  int function0() { return 0; }
+  int function0();
 
-  int function1(int a) { return a; }
+  int function1(int a);
 
-  int function2(int a, int b) { return a + b; }
+  int function2(int a, int b);
 
-  int x() { return m_x; }
+  int x();
 
-  int y() { return m_y; }
+  int y();
 
-  vw::Vector2 do_vector(vw::Vector3 const& a) { return vw::math::subvector(a, 0, 2); }
+  vw::Vector2 do_vector(vw::Vector3 const& a);
 };
 
 }} // namespace algorithm,mvp
