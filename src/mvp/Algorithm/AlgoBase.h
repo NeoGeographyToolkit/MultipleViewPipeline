a371e267014ce4bfd9738d748ea8c33cf64d03c3
Kyle Husmann <kyle.husmann@gmail.com>
2013-02-26 07:27:27 -0800

    Added generalization for AlgoBase

--- src/mvp/Algorithm/AlgoBase.h
+++ src/mvp/Algorithm/AlgoBase.h
@@ -1,3 +1,5 @@
+#ifndef BOOST_PP_IS_ITERATING
+
 #ifndef __MVP_ALGORITHM_ALGOBASE_H__
 #define __MVP_ALGORITHM_ALGOBASE_H__
 
@@ -6,6 +8,10 @@
 #include <boost/functional/factory.hpp>
 #include <boost/function.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/preprocessor/iteration/iterate.hpp>
+#include <boost/preprocessor/repetition/enum_trailing_params.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/repetition/enum_trailing_binary_params.hpp>
 
 #include <vw/Core/Exception.h>
 
@@ -14,58 +20,11 @@ namespace algorithm {
 
 template <class ConstructT> struct AlgoBase;
 
-template <class AlgoT, class T0, class T1>
-struct AlgoBase<AlgoT(T0, T1)> {
-  typedef AlgoT algorithm_type;
+#define MVP_ALGORITHM_MAX_ARITY 10
 
-  private:
-
-    static std::map<std::string, boost::function<AlgoT*(T0, T1)> >& factory_map() {
-      static std::map<std::string, boost::function<AlgoT*(T0, T1)> > map;
-      return map; 
-    }
-
-    static boost::function<AlgoT*(std::string const&, T0, T1)>& octave_factory() {
-      static boost::function<AlgoT*(std::string const&, T0, T1)> factory;
-      return factory;
-    }
-
-    boost::shared_ptr<AlgoT> m_impl;
-
-  protected:
-    AlgoBase() {}
-
-    AlgoBase(std::string const& algo_name, bool use_octave, T0 a0, T1 a1) {
-      if (use_octave) {
-        VW_ASSERT(octave_factory(), vw::LogicErr() << "No octave impl factory registered");
-        m_impl.reset(octave_factory()(algo_name, a0, a1));
-      } else {
-        VW_ASSERT(factory_map().count(algo_name), vw::ArgumentErr() << "Type not found in constructor map");
-        m_impl.reset(factory_map()[algo_name](a0, a1));
-      }        
-    }
-
-    boost::shared_ptr<AlgoT> impl() { 
-      VW_ASSERT(m_impl, vw::LogicErr() << "Method not defined in derived algorithm class");
-      return m_impl;
-    }
-
-    boost::shared_ptr<AlgoT> impl() const { 
-      VW_ASSERT(m_impl, vw::LogicErr() << "Method not defined in derived algorithm class");
-      return m_impl;
-    }
-
-  public:
-    template <class DerivedT>
-    static void register_algorithm(std::string const& name) {
-      factory_map()[name] = boost::factory<DerivedT*>();
-    }
-
-    template <class FactoryT>
-    static void register_octave_factory() {
-      octave_factory() = boost::factory<FactoryT*>();
-    }
-};
+#define BOOST_PP_ITERATION_LIMITS (0, MVP_ALGORITHM_MAX_ARITY)
+#define BOOST_PP_FILENAME_1 <mvp/Algorithm/AlgoBase.h>
+#include BOOST_PP_ITERATE()
 
 template <class DerivedT>
 struct AlgoRegistrar {
@@ -94,3 +53,64 @@ struct OctaveAlgoRegistrar {
 }} // namespace mvp, algorithm
 
 #endif
+
+#else // BOOST_PP_IS_ITERATING
+
+#define N BOOST_PP_ITERATION()
+
+template <class AlgoT BOOST_PP_ENUM_TRAILING_PARAMS(N, class T)>
+struct AlgoBase<AlgoT(BOOST_PP_ENUM_PARAMS(N, T))> {
+  typedef AlgoT algorithm_type;
+
+  private:
+
+    static std::map<std::string, boost::function<AlgoT*(BOOST_PP_ENUM_PARAMS(N, T))> >& factory_map() {
+      static std::map<std::string, boost::function<AlgoT*(BOOST_PP_ENUM_PARAMS(N, T))> > map;
+      return map; 
+    }
+
+    static boost::function<AlgoT*(std::string const& BOOST_PP_ENUM_TRAILING_PARAMS(N, T))>& octave_factory() {
+      static boost::function<AlgoT*(std::string const& BOOST_PP_ENUM_TRAILING_PARAMS(N, T))> factory;
+      return factory;
+    }
+
+    boost::shared_ptr<AlgoT> m_impl;
+
+  protected:
+    AlgoBase() {}
+
+    AlgoBase(std::string const& algo_name, bool use_octave BOOST_PP_ENUM_TRAILING_BINARY_PARAMS(N, T, a)) {
+      if (use_octave) {
+        VW_ASSERT(octave_factory(), vw::LogicErr() << "No octave impl factory registered");
+        m_impl.reset(octave_factory()(algo_name BOOST_PP_ENUM_TRAILING_PARAMS(N, a)));
+      } else {
+        VW_ASSERT(factory_map().count(algo_name), vw::ArgumentErr() << "Type not found in constructor map");
+        m_impl.reset(factory_map()[algo_name](BOOST_PP_ENUM_PARAMS(N, a)));
+      }        
+    }
+
+    boost::shared_ptr<AlgoT> impl() { 
+      VW_ASSERT(m_impl, vw::LogicErr() << "Method not defined in derived algorithm class");
+      return m_impl;
+    }
+
+    boost::shared_ptr<AlgoT> impl() const { 
+      VW_ASSERT(m_impl, vw::LogicErr() << "Method not defined in derived algorithm class");
+      return m_impl;
+    }
+
+  public:
+    template <class DerivedT>
+    static void register_algorithm(std::string const& name) {
+      factory_map()[name] = boost::factory<DerivedT*>();
+    }
+
+    template <class FactoryT>
+    static void register_octave_factory() {
+      octave_factory() = boost::factory<FactoryT*>();
+    }
+};
+
+#undef N
+
+#endif // BOOST_PP_IS_ITERATING
