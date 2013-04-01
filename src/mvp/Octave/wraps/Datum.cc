f42568701455ef1050c550f3454172868e639fbd
Kyle Husmann <kyle.husmann@gmail.com>
2013-04-01 07:16:32 -0700

    Added geodetic_to_cartesian to Datum wrap

--- src/mvp/Octave/wraps/Datum.cc
+++ src/mvp/Octave/wraps/Datum.cc
@@ -3,6 +3,7 @@
 
 BEGIN_MVP_WRAPPER(Datum, vw::cartography::Datum, MVP_WRAPPER_NO_ARGS)
   MVP_WRAPPER_CONSTRUCTOR((std::string))
-  MVP_WRAPPER(semi_major_axis, (double))
-  MVP_WRAPPER(semi_minor_axis, (double))
+  MVP_WRAPPER_C(semi_major_axis, (double))
+  MVP_WRAPPER_C(semi_minor_axis, (double))
+  MVP_WRAPPER_C(geodetic_to_cartesian, (vw::Vector3)(vw::Vector3 const&))
 END_MVP_WRAPPER()
